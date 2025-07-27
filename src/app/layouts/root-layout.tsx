63ce77acf14f2e7ef7e9dd0076ddd803ab589af6
Maksim Ushakov <jenadka@bk.ru>
2025-07-27 14:57:12 +0500

    feat: mutate login

--- src/app/layouts/root-layout.tsx
+++ src/app/layouts/root-layout.tsx
@@ -1,10 +1,11 @@
 import s from "./root-layout.module.scss"
 import { Outlet } from "@tanstack/react-router";
 import { Header } from "../../shared/ui/header/header.tsx";
+import { LoginButton } from "../../features/auth/ui/login-button/login-button.tsx";
 
 export const RootLayout = () => (
   <>
-    <Header renderAccountBar={() => <div>Account</div>} />
+    <Header renderAccountBar={() => <LoginButton /> } />
     <div className={s.container}>
       <Outlet />
     </div>
